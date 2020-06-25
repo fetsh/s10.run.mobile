0f0cf0a3c5330a4c20a3810ea686793070af7914
Eliyahu at Netanya <fetshme@gmail.com>
2020-06-25 16:48:47 +0300

    Add bootstrap

--- s10.run.mobile.user.js
+++ s10.run.mobile.user.js
@@ -15,8 +15,11 @@
 // @grant GM_setValue
 // @grant GM_deleteValue
 // @grant GM_listValues
+// @grant GM_xmlhttpRequest
 // @grant GM_log
 // @run-at document-start
+// ==/UserScript==
+
 
 (function (window) {
     "use strict";
@@ -27,7 +30,11 @@
         downloadURL: 'https://raw.githubusercontent.com/fetsh/s10.run.mobile/master/s10.run.mobile.user.js',
 
         initialize: function () {
-            S10.checkUpdate(true); // check if S10Script is up to date.
+            S10.addMeta();
+            S10.onEvent(window, 'DOMContentLoaded', S10.addNavBar);
+
+            // S10.checkUpdate(true); // check if S10Script is up to date.
+
         },
         getVersion: function () {
             return Number(S10.version);
@@ -51,6 +58,84 @@
             } else {
                 return null;
             }
+        },
+        decodeURI: function (str) {
+            return decodeURIComponent(str);
+        },
+        encodeURI: function (str) {
+            return encodeURIComponent(str);
+        },
+        encodeHTML: function (str) {
+            return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+        },
+        decodeHTML: function (str) {
+            return String(str).replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
+        },
+        serialize: function (obj) {
+            if (typeof obj == 'object') {
+                var arr = [];
+                for (var prop in obj) {
+                    if (obj.hasOwnProperty(prop))
+                        arr.push(prop + '=' + S10.encodeURI(obj[prop]));
+                }
+                return arr.join('&');
+            }
+            return obj;
+        },
+        unserialize: function (str) {
+            str = S10.decodeHTML(str);
+            var arr = str.split('&');
+            var obj = {};
+            arr.forEach(function (entry) {
+                if (entry !== '' && entry.split('=')) {
+                    var splits = entry.split('=');
+                    obj[splits[0]] = S10.decodeURI(splits[1]);
+                }
+            });
+            return obj;
+        },
+        unsetProperties: function (obj, props) {
+            props = (typeof props == 'string') ? props.split(',') : props;
+            for (var i = 0; i < props.length; i++) {
+                var prop = props[i];
+                if (obj.hasOwnProperty(prop)) {
+                    delete obj[prop];
+                }
+            }
+            return obj;
+        },
+        setProperties: function (obj1, obj2) {
+            for (var prop in obj2) {
+                if (obj2.hasOwnProperty(prop))
+                    obj1[prop] = obj2[prop];
+            }
+            return obj1;
+        },
+        request: function (settings) {
+            settings.url = settings.url || '';
+            settings.method = settings.method || 'GET';
+            settings.headers = settings.headers || {};
+            settings.timeout = settings.timeout || 2e4; // 20s
+            if (settings.data || settings.method == 'POST') {
+                settings.method = 'POST';
+                settings.data = S10.serialize(settings.data || {});
+                settings.headers = S10.setProperties(settings.headers, {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                });
+            }
+
+            // override to integrate log
+            settings._onload = settings.onload;
+            settings.onload = function (xhr) {
+                console.log('S10 xhr', xhr);
+                settings._onload(xhr);
+            };
+
+            // Request with GM API
+            // doc: http://tinyurl.com/2t7wbr
+            GM_xmlhttpRequest(settings);
+
         },
         onEvent: function (element, type, listener, bubbles) {
             if (window.addEventListener) { // For all major browsers, except IE 8 and earlier
@@ -108,12 +193,12 @@
         notification: function (message, delay) {
             console.log(message)
             // // remove old notification
-            // Aak.removeElement('#aak-notice-frame');
+            // S10.removeElement('#S10-notice-frame');
 
             // // add new notification
-            // Aak.createElement({
+            // S10.createElement({
             //     tag: 'iframe',
-            //     id: 'aak-notice-frame',
+            //     id: 'S10-notice-frame',
             //     style: 'position:fixed; z-index:999999; top:10px; left:10px;',
             //     width: '360px',
             //     height: '120px',
@@ -125,7 +210,7 @@
             //     callback: function (self) {
 
             //         // manually remove
-            //         Aak.onEvent(window, "message", function (event) {
+            //         S10.onEvent(window, "message", function (event) {
             //             if (event.data == "removeNotification") {
             //                 self.remove();
             //             }
@@ -138,6 +223,25 @@
             //     }
             // });
         },
+        addMeta: function () {
+            // <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            // <meta name="viewport" content="width=device-width, initial-scale=1">
+            var meta1 = document.createElement('meta');
+            meta1.httpEquiv = "X-UA-Compatible";
+            meta1.content = "IE=edge";
+            var meta2 = document.createElement('meta');
+            meta2.name = "viewport";
+            meta2.content = "width=device-width, initial-scale=1";
+            document.getElementsByTagName('head')[0].appendChild(meta1);
+            document.getElementsByTagName('head')[0].appendChild(meta2);
+        },
+        addNavBar: function () {
+            var nav = document.createElement('nav');
+            nav.setAttribute("class", 'navbar navbar-default navbar-fixed-top navbar-inverse');
+            nav.setAttribute("role", 'navigation')
+            nav.innerHTML = "<div class='container-fluid'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='/'>s10</a></div><div class='navbar-collapse collapse'><ul class='nav navbar-nav '></ul><ul class='nav navbar-nav navbar-right'><li><p class='navbar-text'>@fetshme (Ilia Zemskov)</p></li><li><a rel='nofollow' data-method='delete' href='/'>Log Out</a></li></ul></div></div>"
+            document.body.appendChild(nav);
+        }
     };
 
     S10.initialize();
